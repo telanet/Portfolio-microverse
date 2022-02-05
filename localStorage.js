c16895e6fe3a9c4526ad06882a04fbf3c83cc6b9
keza681 <lindakeza19@gmail.com>
2022-02-05 11:27:10 +0200

    Correct localstorage.js

--- localStorage.js
+++ localStorage.js
@@ -1,10 +1,10 @@
-function saveInput(){
-let name, email , message;
-name = document.getElementById('name').value
-email = document.getElementById('email').value
-message = document.getElementById('message').value
+function saveInput() {
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
 
-localStorage.setItem('name',name);
-localStorage.setItem('email',email);
-localStorage.setItem('message',message);
+  localStorage.setItem('name', name);
+  localStorage.setItem('email', email);
+  localStorage.setItem('message', message);
 }
+saveInput();
